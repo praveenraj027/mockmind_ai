542dee3badacf02b99d003de7bfa71f04ee3b0f9
Praveen Rajak <praveenrajak0506@gmail.com>
2025-12-19 00:06:56 +0530

    Started home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,21 +1,40 @@
 import { Button } from '@/components/ui/button';
+import { dummyInterviews } from '@/constants';
 import Image from 'next/image';
 import Link from 'next/link';
 import react from 'react';
+import InterviewCard from '../components/InterviewCard';
 
 const Page = () => {
   return (
     <>
-    <section className='card-cta'>
-      <div className='flex flex-col gap-6 max-w-lg'>
-        <h2>Get Interview ready with AI-powered practise and Feedback</h2>
-        <p className='text-lg'>Practise on real interview quetions and get instant feedback</p>
-        <Button asChild className='btn-primary max-sm:w-full'>
-          <Link href="/interview">Start an Interview</Link>
-        </Button>
-      </div>
-      <Image src="/robot.png" alt='Robot' height={400} width={400} className='max-sm:hidden' />
-    </section>
+      <section className='card-cta'>
+        <div className='flex flex-col gap-6 max-w-lg'>
+          <h2>Get Interview ready with AI-powered practise and Feedback</h2>
+          <p className='text-lg'>Practise on real interview quetions and get instant feedback</p>
+          <Button asChild className='btn-primary max-sm:w-full'>
+            <Link href="/interview">Start an Interview</Link>
+          </Button>
+        </div>
+        <Image src="/robot.png" alt='Robot' height={400} width={400} className='max-sm:hidden' />
+      </section>
+      <section className='flex flex-col gap-6 mt-8'>
+        <h2>Your interviews</h2>
+        <div className='interview-section '>
+          {dummyInterviews.map((interview) => (
+            <InterviewCard {...interview} />
+          ))}
+        </div>
+      </section>
+      <section className='flex flex-col gap-6 mt-8'>
+        <h2>Take an interview</h2>
+        <div className='interview-section'>
+          {dummyInterviews.map((interview) => (
+            <InterviewCard {...interview} />
+          ))}
+          {/* <p>You haven't taken any interviews yet.</p> */}
+        </div>
+      </section>
     </>
   )
 }
---
+++ app/components/InterviewCard.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const InterviewCard = () => {
+    return (
+        <div>Interview Card</div>
+    )
+}
+
+export default InterviewCard;
